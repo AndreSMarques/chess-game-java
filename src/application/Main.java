adf3f4ed76a24840e0f7f6e07b8c8178f9540ae3
Andre Soares Marques <andremarques02345@gmail.com>
2024-10-16 10:40:09 -0300

    feat: implement chess layer and printing the board

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,13 @@
 package application;
 
+import chess.ChessMatch;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ChessMatch chessMatch = new ChessMatch();
+
+        UI.printBoard(chessMatch.getPieces());
+
     }
 }
